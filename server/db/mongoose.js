4a0524ed9a17a06716c792a179f695a92261329c
Abhishek <abhisheksinghal7599@gmail.com>
2018-10-14 05:41:05 +0530

    Setup separate database for test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 var mongoDB = 'mongodb://abhisheksinghal:M+29ko+lab@ds141221.mlab.com:41221/todoapp';
 mongoose.Promise = global.Promise;
 var options =  {useNewUrlParser: true};
-mongoose.connect(mongoDB, options);
+// mongoose.connect(mongoDB, options);
 // mongoose.connect('mongodb://localhost:27017/TodoApp', options);
+mongoose.connect(process.env.MONGODB_URI, options);
 // console.log("Process environment are : ");
 // console.log(JSON.stringify(process.env, undefined, 2));
 
