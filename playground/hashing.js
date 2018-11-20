c2f7b89dafacdc60fa59a43ff09e5a2af3e1bccb
Abhishek <abhisheksinghal7599@gmail.com>
2018-11-21 03:11:50 +0530

    Add password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,28 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 4
-};
+var password = 'abc123!'
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    });
+});
 
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$Px2sDEhwyQUbUCPZJe79lOO7l/vNDDdEwrMPocueQKdjYTwxHCngW';
+bcrypt.compare(password, hashedPassword, (err, res) => {
+    console.log(res);
+});
+// var data = {
+//     id: 4
+// };
+
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 
 // var message = 'I am the master of the universe';
 // var hash = SHA256(message).toString();
@@ -34,4 +47,4 @@ console.log('decoded', decoded);
 //     console.log('Data not changed!');
 // }else{
 //     console.log('Date changed. Do not trust!');
-// }
+// }!
