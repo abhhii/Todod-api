300a7f8ac442562186d7290a1eb067f57c9c0792
Abhishek <abhisheksinghal7599@gmail.com>
2018-11-21 15:07:20 +0530

    Add tests for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -94,18 +94,6 @@ app.patch('/todos/:id', (req,res) => {
     })
 });
 
-// app.post('/todos', (req, res) => {
-//     console.log(req.body);
-//      var todo = new Todo({
-//          text: req.body.text
-//      }); 
-//      todo.save().then((doc) => {
-//          res.send(doc);
-//      }, (err) => {
-//          res.status(400).send(err);
-//       }); 
-// });
-
 app.post('/users',(req,res) => {
     var body = _.pick(req.body, ['email','password']);
     var user = new User(body);
